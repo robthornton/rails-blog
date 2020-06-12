59a8e88ca2b3c7e73b7ae7dc6d8b32aba77d6b1f
Rob Thornton <rob.thornton@shopify.com>
2020-06-12 08:55:28 -0400

    Use built-in password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,9 @@
 
 class UsersController < ApplicationController
   def create
-    if user_params[:password] != user_params[:password_confirmation]
-      redirect_to(signup_path, flash: { error: 'Passwords do not match!' })
-      return
-    end
-
-    user = User.create(user_params.except(:password_confirmation))
+    user = User.create(user_params)
     unless user.valid?
-      # TODO Properly log errors
+      # TODO: Properly log errors
       user.errors.each do |key, msg|
         puts "#{key}: #{msg} #{user_params[:email]}"
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_secure_password
   validates(:name, presence: true)
   validates(:email, presence: true, uniqueness: { case_sensitive: false })
+  attr_accessor(:password_confirmation)
 end

ed26dfbbb5548c9796031eced2ecdffad04cd596
Rob Thornton <rob.thornton@shopify.com>
2020-06-13 11:22:31 -0400

    Bootstrap blog controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  root('blog#index')
+
   get('login', to: 'sessions#login')
   post('login', to: 'sessions#new')
   get('logout', to: 'sessions#delete')

d546abd8b555762411b051ee5e11a2d6a0fa06a5
Rob Thornton <rob.thornton@shopify.com>
2020-06-13 12:24:24 -0400

    Rename blog to post

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class PostController < ApplicationController
+  before_action(:require_login, only: [:new])
+
+  def index
+  end
+
+  def new
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = 'You must be logged in to create a new blog post'
+      redirect_to(login_url)
+    end
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  root('blog#index')
+  root('post#index')
 
+  # Blog/posts
+  namespace(:post) do
+    get(:new, to: 'post#new')
+  end
+
+  # Login
   get('login', to: 'sessions#login')
   post('login', to: 'sessions#new')
   get('logout', to: 'sessions#delete')
 
+  # Sign up
   get('signup', to: 'users#new')
   post('signup', to: 'users#create')
 end

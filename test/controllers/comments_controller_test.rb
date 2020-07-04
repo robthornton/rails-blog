090a4edd65e23d3d5b625d9a866ad89aac57b5ef
Rob Thornton <rob.thornton@shopify.com>
2020-07-04 17:20:37 -0400

    Scaffold comments controller

---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CommentsControllerTest < ActionDispatch::IntegrationTest
+  test 'create adds new comment to post' do
+    skip
+  end
+end
